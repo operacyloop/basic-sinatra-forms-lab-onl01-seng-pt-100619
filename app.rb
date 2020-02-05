263a3111c409dd6cf6132d565a306f4f1a8ff38c
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-02-05 23:07:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,28 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  # post '/Team' do
+  #   "Hello World"
+  #   erb :team
+     
+
+ post '/team' do
+   @team = params[:team]
+   @coach = params[:coach]
+   @pg = @params[:pg]
+   @sg = @params[:sq]
+   @sf = @params[:sf]
+   @pf = @params[:pf]
+   @c = @params[:c]
+   erb :team
+  end
 
 end
+
+
+
+
