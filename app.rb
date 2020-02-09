0a1958988744127788bc1a88639bb9897bc26f40
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-02-09 15:11:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
   end
 
  post '/team' do
-      @name = params["name"]	
+    @name = params["name"]	
     @coach = params["coach"]	
     @pg = params["pg"]	
     @sg = params["sg"]	
     @sf = params["sf"]	
     @pf = params["pf"]	
-    @center = params["c"]
+    @c = params["c"]
    erb :team
   end
 
